c9244be1c63d5f7bba47e555aaa9cee28f68e832
a-d-smith <andrewsmithweb@gmail.com>
2018-04-10 13:16:50 +0100

    Separated template analysis logic into functions

--- larpandora/LArPandoraAnalysis/ConsolidatedPFParticleAnalysisTemplate_module.cc
+++ larpandora/LArPandoraAnalysis/ConsolidatedPFParticleAnalysisTemplate_module.cc
@@ -7,6 +7,10 @@
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Core/EDAnalyzer.h"
 
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/Shower.h"
+
 #include <string>
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -20,6 +24,12 @@ namespace lar_pandora
 class ConsolidatedPFParticleAnalysisTemplate : public art::EDAnalyzer
 {
 public:
+    typedef art::Handle< std::vector<recob::PFParticle> > PFParticleHandle;
+    typedef std::map< size_t, art::Ptr<recob::PFParticle> > PFParticleIdMap;
+    typedef std::vector< art::Ptr<recob::PFParticle> > PFParticleVector;
+    typedef std::vector< art::Ptr<recob::Track> > TrackVector;
+    typedef std::vector< art::Ptr<recob::Shower> > ShowerVector;
+
     /**
      *  @brief  Constructor
      *
@@ -42,6 +52,34 @@ public:
     void analyze(const art::Event &evt);
 
 private:
+    /**
+     *  @brief  Produce a mapping from PFParticle ID to the art ptr to the PFParticle itself for fast navigation
+     *
+     *  @param  pfParticleHandle the handle for the PFParticle collection
+     *  @param  pfParticleMap the mapping from ID to PFParticle
+     */
+    void GetPFParticleIdMap(const PFParticleHandle &pfParticleHandle, PFParticleIdMap &pfParticleMap);
+
+    /**
+     *  @brief  Produce a mapping from PFParticle ID to the art ptr to the PFParticle itself for fast navigation
+     *
+     *  @param  pfParticleMap the mapping from ID to PFParticle
+     *  @param  crParticles a vector to hold the top-level PFParticles reconstructed under the cosmic hypothesis
+     *  @param  nuParticles a vector to hold the final-states of the reconstruced neutrino
+     */
+    void GetFinalStatePFParticleVectors(const PFParticleIdMap &pfParticleMap, PFParticleVector &crParticles, PFParticleVector &nuParticles);
+
+    /**
+     *  @brief  Collect associated tracks and showers to particles in an input particle vector
+     *
+     *  @param  particles a vector holding PFParticles from which to find the associated tracks and showers
+     *  @param  pfParticleHandle the handle for the PFParticle collection
+     *  @param  evt the art event to analyze
+     *  @param  tracks a vector to hold the associated tracks
+     *  @param  showers a vector to hold the associated showers
+     */
+    void CollectTracksAndShowers(const PFParticleVector &particles, const PFParticleHandle &pfParticleHandle, const art::Event &evt, TrackVector &tracks, ShowerVector &showers);
+
     std::string m_pandoraLabel;         ///< The label for the pandora producer
     std::string m_trackLabel;           ///< The label for the track producer from PFParticles
     std::string m_showerLabel;          ///< The label for the shower producer from PFParticles
@@ -62,10 +100,6 @@ DEFINE_ART_MODULE(ConsolidatedPFParticleAnalysisTemplate)
 #include "art/Framework/Services/Optional/TFileDirectory.h"
 #include "canvas/Persistency/Common/FindManyP.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
-
-#include "lardataobj/RecoBase/PFParticle.h"
-#include "lardataobj/RecoBase/Track.h"
-#include "lardataobj/RecoBase/Shower.h"
 
 #include "Pandora/PdgTable.h"
 
@@ -93,39 +127,69 @@ void ConsolidatedPFParticleAnalysisTemplate::reconfigure(fhicl::ParameterSet con
 void ConsolidatedPFParticleAnalysisTemplate::analyze(const art::Event &evt)
 {
     // Collect the PFParticles from the event
-    art::Handle< std::vector<recob::PFParticle> > pfParticleHandle;
+    PFParticleHandle pfParticleHandle;
     evt.getByLabel(m_pandoraLabel, pfParticleHandle);
    
     if (!pfParticleHandle.isValid())
     {
-        mf::LogDebug("LArPandora") << "  Failed to find the PFParticles." << std::endl;
+        mf::LogDebug("ConsolidatedPFParticleAnalysisTemplate") << "  Failed to find the PFParticles." << std::endl;
         return;
     }
 
     // Produce a map of the PFParticle IDs for fast navigation through the hierarchy
-    std::map< size_t, art::Ptr<recob::PFParticle> > pfParticleMap;
-    for (unsigned int i = 0; i < pfParticleHandle->size(); ++i)
-    {
-        const art::Ptr<recob::PFParticle> pParticle(pfParticleHandle, i);
-        pfParticleMap.insert(std::map< size_t, art::Ptr<recob::PFParticle> >::value_type(pParticle->Self(), pParticle));
-    }
+    PFParticleIdMap pfParticleMap;
+    this->GetPFParticleIdMap(pfParticleHandle, pfParticleMap);
     
     // Produce two PFParticle vectors containing final-state particles:
     // 1. Particles identified as cosmic-rays - recontructed under cosmic-hypothesis
     // 2. Daughters of the neutrino PFParticle - reconstructed under the neutrino hypothesis
     std::vector< art::Ptr<recob::PFParticle> > crParticles;
     std::vector< art::Ptr<recob::PFParticle> > nuParticles;
+    this->GetFinalStatePFParticleVectors(pfParticleMap, crParticles, nuParticles);
+
+    // Use as required!
+    // -----------------------------
+    //   What follows is an example showing how one might access the reconstructed neutrino final-state tracks and showers
     
+    // These are the vectors to hold the tracks and showers for the final-states of the reconstructed neutrino
+    std::vector< art::Ptr<recob::Track> > tracks;
+    std::vector< art::Ptr<recob::Shower> > showers;
+    this->CollectTracksAndShowers(nuParticles, pfParticleHandle, evt, tracks, showers);
+
+    // Print a summary of the consolidated event
+    std::cout << "Consolidated event summary:" << std::endl;
+    std::cout << "  - Number of primary cosmic-ray PFParticles   : " << crParticles.size() << std::endl;
+    std::cout << "  - Number of neutrino final-state PFParticles : " << nuParticles.size() << std::endl;
+    std::cout << "    ... of which are track-like   : " << tracks.size() << std::endl;
+    std::cout << "    ... of which are showers-like : " << showers.size() << std::endl;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void ConsolidatedPFParticleAnalysisTemplate::GetPFParticleIdMap(const PFParticleHandle &pfParticleHandle, PFParticleIdMap &pfParticleMap)
+{
     for (unsigned int i = 0; i < pfParticleHandle->size(); ++i)
     {
         const art::Ptr<recob::PFParticle> pParticle(pfParticleHandle, i);
+        if (!pfParticleMap.insert(PFParticleIdMap::value_type(pParticle->Self(), pParticle)).second)
+        {
+            throw cet::exception("ConsolidatedPFParticleAnalysisTemplate") << "  Unable to get PFParticle ID map, the input PFParticle collection has repeat IDs!";
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+    
+void ConsolidatedPFParticleAnalysisTemplate::GetFinalStatePFParticleVectors(const PFParticleIdMap &pfParticleMap, PFParticleVector &crParticles, PFParticleVector &nuParticles)
+{
+    for (PFParticleIdMap::const_iterator it = pfParticleMap.begin(); it != pfParticleMap.end(); ++it)
+    {
+        const art::Ptr<recob::PFParticle> pParticle(it->second);
 
         // Only look for primary particles
         if (!pParticle->IsPrimary()) continue;
 
         // Check if this particle is identified as the neutrino
-        // ATTN. We are filling nuParticles under the assumption that there is only one reconstructed neutrino identified per event.
-        //       If this is not the case please modify accordingly
         const int pdg(pParticle->PdgCode());
         const bool isNeutrino(std::abs(pdg) == pandora::NU_E || std::abs(pdg) == pandora::NU_MU || std::abs(pdg) == pandora::NU_TAU);
 
@@ -136,30 +200,33 @@ void ConsolidatedPFParticleAnalysisTemplate::analyze(const art::Event &evt)
             continue;
         }
 
+        // ATTN. We are filling nuParticles under the assumption that there is only one reconstructed neutrino identified per event.
+        //       If this is not the case please handle accordingly
+        if (!nuParticles.empty())
+        {
+            throw cet::exception("ConsolidatedPFParticleAnalysisTemplate") << "  This event contains multiple reconstructed neutrinos!";
+        }
+
         // Add the daughters of the neutrino PFParticle to the nuPFParticles vector
         for (const size_t daughterId : pParticle->Daughters())
         {
             if (pfParticleMap.find(daughterId) == pfParticleMap.end())
-                throw cet::exception("LArPandora") << "  Invalid PFParticle collection!";
+                throw cet::exception("ConsolidatedPFParticleAnalysisTemplate") << "  Invalid PFParticle collection!";
 
             nuParticles.push_back(pfParticleMap.at(daughterId));
         }
     }
+}
 
-    // Use as required!
-    // -----------------------------
-    //   What follows is an example showing how to access the reconstructed neutrino final-state tracks and showers
+//------------------------------------------------------------------------------------------------------------------------------------------
     
+void ConsolidatedPFParticleAnalysisTemplate::CollectTracksAndShowers(const PFParticleVector &particles, const PFParticleHandle &pfParticleHandle, const art::Event &evt, TrackVector &tracks, ShowerVector &showers)
+{
     // Get the associations between PFParticles and tracks/showers from the event
     art::FindManyP< recob::Track > pfPartToTrackAssoc(pfParticleHandle, evt, m_trackLabel);
     art::FindManyP< recob::Shower > pfPartToShowerAssoc(pfParticleHandle, evt, m_showerLabel);
    
-    // These are the vectors to hold the tracks and showers for the final-states of the reconstructed neutrino
-    std::vector< art::Ptr<recob::Track> > tracks;
-    std::vector< art::Ptr<recob::Shower> > showers;
-
-    // For each PFParticle, find associated tracks and showers
-    for (const art::Ptr<recob::PFParticle> &pParticle : nuParticles)
+    for (const art::Ptr<recob::PFParticle> &pParticle : particles)
     {
         const std::vector< art::Ptr<recob::Track> > associatedTracks(pfPartToTrackAssoc.at(pParticle.key()));
         const std::vector< art::Ptr<recob::Shower> > associatedShowers(pfPartToShowerAssoc.at(pParticle.key()));
@@ -169,7 +236,7 @@ void ConsolidatedPFParticleAnalysisTemplate::analyze(const art::Event &evt)
         // Check if the PFParticle has no associated tracks or showers
         if (nTracks == 0 && nShowers == 0)
         {
-            mf::LogDebug("LArPandora") << "  No tracks or showers were associated to PFParticle " << pParticle->Self() << std::endl;
+            mf::LogDebug("ConsolidatedPFParticleAnalysisTemplate") << "  No tracks or showers were associated to PFParticle " << pParticle->Self() << std::endl;
             continue;
         }
 
@@ -187,15 +254,8 @@ void ConsolidatedPFParticleAnalysisTemplate::analyze(const art::Event &evt)
             continue;
         }
 
-        throw cet::exception("LArPandora") << "  There were " << nTracks << " tracks and " << nShowers << " showers associated with PFParticle " << pParticle->Self();
+        throw cet::exception("ConsolidatedPFParticleAnalysisTemplate") << "  There were " << nTracks << " tracks and " << nShowers << " showers associated with PFParticle " << pParticle->Self();
     }
-
-    // Print a summary of the consolidated event
-    std::cout << "Consolidated event summary:" << std::endl;
-    std::cout << "  - Number of primary cosmic-ray PFParticles   : " << crParticles.size() << std::endl;
-    std::cout << "  - Number of neutrino final-state PFParticles : " << nuParticles.size() << std::endl;
-    std::cout << "    ... of which are track-like   : " << tracks.size() << std::endl;
-    std::cout << "    ... of which are showers-like : " << showers.size() << std::endl;
 }
 
 } //namespace lar_pandora
